8b88bf06efdfae1145f01264d37e2b4f35f4a0d6
David Malpartida <dmalpartida@gmail.com>
2019-09-06 17:49:49 -0500

    adding two fragments

---
+++ app/src/main/java/com/globalredland/converter/ui/equivalente/EquivalenteFragment.java
@@ -0,0 +1,35 @@
+package com.globalredland.converter.ui.equivalente;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.globalredland.converter.R;
+
+public class EquivalenteFragment extends Fragment {
+
+    private EquivalenteViewModel equivalenteViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        equivalenteViewModel =
+                ViewModelProviders.of(this).get(EquivalenteViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_equivalente, container, false);
+        final TextView textView = root.findViewById(R.id.text_equivalente);
+        equivalenteViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
--- app/src/main/java/com/globalredland/converter/ui/home/HomeFragment.java
+++ app/src/main/java/com/globalredland/converter/ui/home/HomeFragment.java
@@ -4,27 +4,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.globalredland.converter.R;
+import com.globalredland.converter.ui.converter.ConverterFragment;
+import com.globalredland.converter.ui.equivalente.EquivalenteFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+                             final ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
+
+        final NavigationView navigationView = getActivity().findViewById(R.id.nav_view);
+
+        Button btnConverter = root.findViewById(R.id.button_unit_converter);
+        Button btnEquivalente = root.findViewById(R.id.button_unit_equivalent);
+
+        btnConverter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                navigationView.getMenu().getItem(1).setChecked(true);
+
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.nav_host_fragment, new ConverterFragment());
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.addToBackStack(null);
+                ft.commit();
+
+            }
+        });
+
+        btnEquivalente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                navigationView.getMenu().getItem(2).setChecked(true);
+
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.nav_host_fragment, new EquivalenteFragment());
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
         homeViewModel.getText().observe(this, new Observer<String>() {
+
             @Override
             public void onChanged(@Nullable String s) {
 //                textView.setText(s);
